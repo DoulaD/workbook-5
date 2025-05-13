d44f6c27cd5509196f86bbb061b1526967ac9440
DoulaD <Douladoumbia@gmail.com>
2025-05-13 15:25:38 -0400

    Fields and classes

---
+++ AssetManager/src/main/java/com/pluralsight/Asset.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.util.concurrent.CompletionStage;
+
+public class Asset {
+
+    private String description;
+    private String dateAcquired;
+    private double originalCost;
+}
---
+++ AssetManager/src/main/java/com/pluralsight/House.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class House {
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+
